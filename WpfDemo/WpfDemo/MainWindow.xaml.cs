fd80b8ae620797b4ff3a8e928f84af79bc48f8d0
Andrei <apatskou@gmail.com>
2024-06-21 20:23:21 +0300

    218-230

---
+++ WpfDemo/WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button btn = new();
+            btn.Content = "B";
+            Grid.SetRow(btn, 3);
+            Grid.SetColumn(btn, 4);
+
+            Grid grd = (Grid)FindName("mainGrid");
+
+            grd.Children.Add(btn);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World!!!");
+        }
+    }
+}
