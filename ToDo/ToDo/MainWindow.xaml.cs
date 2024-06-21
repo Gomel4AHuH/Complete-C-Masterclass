298814a2a9c41274881c98848ca7730cb72e1eec
Andrei <apatskou@gmail.com>
2024-06-22 00:52:57 +0300

    Added ToDo

---
+++ ToDo/ToDo/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ToDo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddToDoItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            string todoText = ToDoInput.Text.Trim();
+
+            if (!string.IsNullOrEmpty(todoText))
+            {
+                TextBlock toDoItem = new()
+                {
+                    Text = todoText,
+                    Margin = new Thickness(5)
+                };
+                ToDoList.Children.Add(toDoItem);
+                ToDoInput.Clear();
+                ToDoInput.Focus();
+            }
+        }
+    }
+}
