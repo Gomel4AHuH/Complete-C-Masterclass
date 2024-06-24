7feb00bf3fe8eb102bcb4f163f5ffb783bfd9261
Andrei <apatskou@gmail.com>
2024-06-24 17:18:29 +0300

    Login page. 251-257

---
+++ InvoiceManagement/InvoiceManagement/LoginView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace InvoiceManagement
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        const string pswrd = "2001";
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string pswrdEntered = PasswordBox.Password;
+            MessageBox.Show((pswrdEntered == pswrd) ? "Correct" : "Incorrect");
+        }
+
+        public void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            LoginButton.IsEnabled = !string.IsNullOrEmpty(PasswordBox.Password);
+        }
+    }
+}
