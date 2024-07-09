84d2c8ed54a773d31a6d18bb679bfacf7fbf688f
Andrei <apatskou@gmail.com>
2024-07-09 17:27:03 +0300

    HelloWorldApp added.

---
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, Andrei!");
+//declare variable
+string myFriendsName;
+//assign a value
+myFriendsName = "Artsiom";
+string myFriendsName2 = "Den";
+//use variable
+Console.WriteLine(myFriendsName);
+//overwriting varialble
+myFriendsName = "Serg";
+Console.WriteLine(myFriendsName);
+Console.ReadKey();
