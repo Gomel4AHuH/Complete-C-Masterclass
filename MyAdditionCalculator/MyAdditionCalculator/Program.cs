409a7d53fd96ccc37ac52d9d1ed833990824708d
Andrei <apatskou@gmail.com>
2024-07-11 19:11:23 +0300

    Added MyAdditionalCalculator

---
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter the first number:");
+
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The first number is {firstNumber}");
+
+Console.WriteLine("Enter the second number:");
+
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The second number is {secondNumber}");
+
+Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}");
+
+Console.ReadKey();
